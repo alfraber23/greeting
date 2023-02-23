47142e57449ac471483e1f8d31063ba68b2e57a0
Beto <josealberto.fb15@gmail.com>
2023-02-23 11:53:14 -0600

    Feat: greeting

---
+++ actserge/src/main/java/org/generation/actserge/ActsergeApplication.java
@@ -0,0 +1,13 @@
+package org.generation.actserge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ActsergeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ActsergeApplication.class, args);
+	}
+
+}
---
+++ actserge/src/test/java/org/generation/actserge/ActsergeApplicationTests.java
@@ -0,0 +1,13 @@
+package org.generation.actserge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ActsergeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
